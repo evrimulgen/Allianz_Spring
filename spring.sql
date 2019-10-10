fd4447706409fae039845ee25599a82e7b5c6d0e
Hakan ÖZER <hakanozer02@gmail.com>
2019-10-10 16:27:27 +0300

    Rest

--- spring.sql
+++ spring.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: localhost
--- Üretim Zamanı: 09 Eki 2019, 19:02:28
+-- Üretim Zamanı: 10 Eki 2019, 15:15:41
 -- Sunucu sürümü: 10.4.6-MariaDB
 -- PHP Sürümü: 7.1.31
 
@@ -48,6 +48,30 @@ INSERT INTO `admin` (`aid`, `name`, `mail`, `pass`, `statu`, `role`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Tablo için tablo yapısı `product`
+--
+
+CREATE TABLE `product` (
+  `pid` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `detail` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `product`
+--
+
+INSERT INTO `product` (`pid`, `title`, `detail`, `price`) VALUES
+(1, 'Buzdolabı', 'Ürün Detayı', 3000),
+(2, 'Buzdolabı', 'Ürün Detayı', 3000),
+(3, 'Audi A5', 'Yorumsuz', 300000),
+(4, 'Audi A3', 'Yorumsuz', 300000),
+(5, 'Audi A3', 'Yorumsuz', 300000);
+
+-- --------------------------------------------------------
+
+--
 -- Tablo için tablo yapısı `user`
 --
 
@@ -77,6 +101,12 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`aid`);
 
 --
+-- Tablo için indeksler `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`pid`);
+
+--
 -- Tablo için indeksler `user`
 --
 ALTER TABLE `user`
@@ -91,6 +121,12 @@ ALTER TABLE `user`
 --
 ALTER TABLE `admin`
   MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `product`
+--
+ALTER TABLE `product`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `user`
